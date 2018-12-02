e17e35f5edaf1ea37e6cd12f9e6fb76ad03f4e39
snayerman <sambackpack@gmail.com>
2018-12-01 17:37:02 -0800

    Add file upload

--- src/Upload/Upload.js
+++ src/Upload/Upload.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Upload = props => {
-   return (
-      <div>UPLOAD DATASET</div>
-   );
+import './Upload.scss';
+
+export default class Upload extends Component {
+   state = {
+      selectedFile: null
+   };
+
+   handleChange = event => {
+      this.setState({selectedFile: event.target.files[0]});
+   }
+
+   render() {
+      return (
+         <div className="upload-body">
+            UPLOAD DATASET
+            <input type="file" onChange={this.handleChange}/>
+         </div>
+      );
+   }
 }
-
-export default Upload;
