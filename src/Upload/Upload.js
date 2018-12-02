abb67e41c63a55910adb1c1825993abebe3c0a8e
snayerman <sambackpack@gmail.com>
2018-12-01 17:57:16 -0800

    File reader

--- src/Upload/Upload.js
+++ src/Upload/Upload.js
@@ -11,11 +11,24 @@ export default class Upload extends Component {
       this.setState({selectedFile: event.target.files[0]});
    }
 
+   renderFileContents = () => {
+      let reader = new FileReader();
+
+      reader.onload = () => {
+         let text = reader.result;
+         console.log(text);
+      }
+
+      reader.readAsText(this.state.selectedFile);
+   }
+
    render() {
       return (
          <div className="upload-body">
             UPLOAD DATASET
             <input type="file" onChange={this.handleChange}/>
+
+            {this.state.selectedFile != null ? this.renderFileContents() : null}
          </div>
       );
    }
