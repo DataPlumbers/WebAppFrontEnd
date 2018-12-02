a89fb920a8502710c5cade2b9345c9e0a66d8cb2
Tony Chen <tonyc856@users.noreply.github.com>
2018-12-01 18:45:51 -0800

    Pulled new changes in Upload from master

--- src/Upload/Upload.js
+++ src/Upload/Upload.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Upload = props => {
-   return (
-      <div>UPLOAD DATASET</div>
-   );
+import './Upload.scss';
+
+export default class Upload extends Component {
+   state = {
+      selectedFile: null
+   };
+
+   handleChange = event => {
+      this.setState({selectedFile: event.target.files[0]});
+   }
+
+   renderFileContents = () => {
+      let reader = new FileReader();
+
+      reader.onload = () => {
+         let text = reader.result;
+         console.log(text);
+      }
+
+      reader.readAsText(this.state.selectedFile);
+   }
+
+   render() {
+      return (
+         <div className="upload-body">
+            UPLOAD DATASET
+            <input type="file" onChange={this.handleChange}/>
+
+            {this.state.selectedFile != null ? this.renderFileContents() : null}
+         </div>
+      );
+   }
 }
-
-export default Upload;
