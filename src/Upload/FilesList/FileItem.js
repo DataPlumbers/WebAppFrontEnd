bdf95ae554ca581743001bf866b7df566bdff294
snayerman <sambackpack@gmail.com>
2018-12-05 12:51:52 -0800

    Render TableCells dynamically

--- src/Upload/FilesList/FileItem.js
+++ src/Upload/FilesList/FileItem.js
@@ -3,25 +3,23 @@ import { CircularProgress, TableCell, TableRow } from '@material-ui/core';
 
 export default class FileItem extends Component {
 
+   renderCells = (ids) => {
+      return ids.map(id => {
+         return <TableCell key={id}>{this.props.file[id]}</TableCell>
+      })
+   }
+   
    render() {
       return (
          <TableRow hover={true}>
-               <TableCell>
-                  {this.props.file.name}
-               </TableCell>
-               <TableCell>
-                  {this.props.file.size}   
-               </TableCell>
-               <TableCell>
-                  {this.props.file.type}        
-               </TableCell>
-               <TableCell className="upload-progress">
-                  <CircularProgress
-                     variant="static"
-                     size={20}
-                     value={80}
-                  />
-               </TableCell>
+            {this.renderCells(["name", "size", "type"])}
+            <TableCell className="upload-progress">
+               <CircularProgress
+                  variant="static"
+                  size={20}
+                  value={80}
+               />
+            </TableCell>
          </TableRow>
       );
    }
