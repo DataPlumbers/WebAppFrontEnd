667d76d6b7c86c0eb05eb1d662068c4693d1f760
Tony Chen <tonyc856@users.noreply.github.com>
2019-02-19 13:05:54 -0800

    Implemented sending dataset and classifcation details to the backend. Minor adjustments on UI.

--- src/Classify/Classify.js
+++ src/Classify/Classify.js
@@ -1,48 +1,69 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import { IconButton, TextField, FormGroup } from '@material-ui/core/';
 import { Add } from '@material-ui/icons/';
-import PropertiesList from './PropertiesList';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
+import PropertiesList from './PropertiesList';
 import './Classify.scss';
 
 export default class Classify extends Component {
-
-   constructor() {
-      super();
-      this.state = {
-         category: "",
-         property: "",
-         properties: []
-      }
-      this.handleChange = this.handleChange.bind(this);
-   }
+   state = {
+      category: "",
+      property: "",
+      properties: []
+   };
 
    addProperty = () => {
       if (this.state.property) {
          let newPropertiesList = this.state.properties.slice();
          newPropertiesList.push(this.state.property);
-
          this.setState({properties: newPropertiesList, property: ""});
       }
-   }
+   };
 
-   handPropertiesListChange = (propertyToRemove) => {
+   handPropertiesListChange = propertyToRemove => {
       let newPropertiesList;
-
       newPropertiesList = this.state.properties.filter(property => {
          return property !== propertyToRemove;
       });
       this.setState({properties: newPropertiesList});
-   }
+   };
 
-   handleChange(event) {
+   handleChange = event => {
       this.setState({ [event.target.name]: event.target.value });
-   }
+   };
+
+   classify = () => {
+      const url = "http://127.0.0.1:8000/upload";
+      const data = new FormData();
+      const files = this.props.location.state.files; // files passed from Upload.js
+      
+      files.forEach(file => {
+         data.append('file', file);
+      });
+      data.append('category', this.state.category); // category name (ex. "Review")
+      data.append('properties', this.state.properties); // list of headers for classification (ex. ['author', 'comment', 'date'])
+
+      Axios.post(url, data).then(response => {
+         
+      });
+   };
 
    render() {
       return (
          <>
-            <h3>Dataset Category</h3>
+            <Grid justify="space-around" alignItems="center" direction="row" container>
+               <Grid item>
+                  <h3>Classify Dataset</h3>
+               </Grid>
+               <Grid item>
+                  <Button variant="contained" color="primary" onClick={this.classify}>
+                     Classify
+                  </Button>
+               </Grid>
+            </Grid>
             <div className="classify content-body">
                <FormGroup>
                   <FormGroup row={true}>
@@ -59,17 +80,23 @@ export default class Classify extends Component {
                      />
                   </FormGroup>
                   <FormGroup row={true}>
-                     <TextField
-                        id="category-property"
-                        name="property"
-                        label="Property"
-                        value={this.state.property}
-                        onChange={this.handleChange}
-                        margin="normal"
-                     />
-                     <IconButton onClick={this.addProperty} aria-label="Add">
-                        <Add />
-                     </IconButton>
+                     <Grid container alignItems="center">
+                        <Grid item>
+                           <TextField
+                              id="category-property"
+                              name="property"
+                              label="Property"
+                              value={this.state.property}
+                              onChange={this.handleChange}
+                              margin="normal"
+                           />
+                        </Grid>
+                        <Grid item>
+                           <IconButton onClick={this.addProperty} aria-label="Add">
+                              <Add />
+                           </IconButton>
+                        </Grid>
+                     </Grid>
                   </FormGroup>
                   <div className="properties-container">
                      <PropertiesList onChange={this.handPropertiesListChange} properties={this.state.properties} />
