32bf5b879ec958a4a35af97582b828a727ff19ac
snayerman <sambackpack@gmail.com>
2019-04-18 00:21:56 -0700

    Redesign and refactor UI for file upload

--- src/Upload/DropArea/DropArea.js
+++ src/Upload/DropArea/DropArea.js
@@ -7,13 +7,27 @@ export default class DropArea extends Component {
       this.props.onDrop(event.target.files);
    };
 
+   toggleUpload = () => {
+      this.input.click();
+   }
+
    render() {
       return (
-         <div className="droparea container">
-            <input className="file-input" type="file" multiple onChange={this.handleChange} accept=".csv" />
-            <div className="upload-icon container">
-               <Icon className="icon" fontSize="large">cloud_upload</Icon>
-               <h3>Select dataset files to upload</h3>
+         <div onClick={this.toggleUpload} className="input-body">
+            <div className="droparea container">
+               <div className="upload-icon container">
+                  <Icon className="icon" fontSize="large">cloud_upload</Icon>
+                  <h3>Select dataset files to upload</h3>
+               </div>
+               <input
+                  className="file-input"
+                  type="file"
+                  multiple
+                  onChange={this.handleChange}
+                  accept=".csv"
+                  ref={input => this.input = input}
+                  style={{ display: 'none' }}
+               />
             </div>
          </div>
       );
