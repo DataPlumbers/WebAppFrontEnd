a47945dc880c5cbf2b527a7f6bbd2a4aa77ee769
Tony Chen <tonyc856@users.noreply.github.com>
2019-05-27 17:24:58 -0700

    Added gitbook link to menu and refactor menu with a drawer.

--- src/NavBar/MenuList/MenuList.js
+++ src/NavBar/MenuList/MenuList.js
@@ -2,56 +2,84 @@ import React, { Component } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import List from '@material-ui/core/List';
 import { Link } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import { Assessment, Info } from '@material-ui/icons';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export default class MenuList extends Component {
    state = {
-      anchorEl: null
+      isDrawerOpen: false
    }
 
-   handleClick = event => {
-      this.setState({ anchorEl: event.currentTarget });
+   toggleDrawer = () => {
+      this.setState({isDrawerOpen: !this.state.isDrawerOpen});
    };
 
-   handleClose = (path) => {
-      this.setState({ anchorEl: null });
+   renderMenuList = (links) => {
+      return links.map(link => {
+         return (
+            link.internal ? this.renderInternalLink(link) : this.renderEnternalLink(link)
+         );
+      });
    };
+
+   renderInternalLink(link) {
+      return (
+         <MenuItem key={link.url} selected={false} onClick={this.toggleDrawer} component={props => <Link to={`/${link.url}`} {...props} />}>
+            {this.renderMenuItems(link)}
+         </MenuItem>
+      );
+   };
+
+   renderEnternalLink(link) {
+      return (
+         <MenuItem 
+            key={link.url} 
+            selected={false} 
+            onClick={this.toggleDrawer} 
+            component="a" href={link.url}
+            target="_blank">
+            {this.renderMenuItems(link)}
+         </MenuItem>
+      );
+   };
+
+   renderMenuItems = (link) => {
+      return (
+         <>
+            <ListItemIcon>{link.icon}</ListItemIcon>
+            <ListItemText primary={link.title} />
+         </>
+      );
+   }
 
    render() {
-      const open = Boolean(this.state.anchorEl);
       const links = [
-         {title: "Upload Datasets", url: "upload"}
-      ]
+         {title: "Data Classifier", url: "upload", icon: <Assessment />, internal: true},
+         {title: "Help", url: "https://dataplumbers123.gitbook.io/data-classifier/", icon: <Info />, internal: false}
+      ];
       
       return (
          <>
-            <IconButton className={this.props.buttonClass} color="inherit" aria-label="Menu" onClick={this.handleClick}>
+            <IconButton className={this.props.buttonClass} color="inherit" aria-label="Menu" onClick={this.toggleDrawer}>
                <MenuIcon />
             </IconButton>
 
-            <Menu
-               anchorEl={this.state.anchorEl}
-               anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left'
-               }}
-               transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'center',
-               }}
-               getContentAnchorEl={null}
-               open={open}
-               onClose={this.handleClose}
-            >
-               {links.map(link => {
-                  return (
-                     <MenuItem key={link.url} selected={false} onClick={this.handleClose} component={props => <Link to={`/${link.url}`} {...props} />}>
-                        {link.title}
-                     </MenuItem>
-                  );
-               })}
-            </Menu>
+            <Drawer open={this.state.isDrawerOpen} onClose={this.toggleDrawer}>
+               <div
+                  tabIndex={0}
+                  role="button"
+                  onClick={ this.toggleDrawer }
+                  onKeyDown={ this.toggleDrawer }
+               >
+                  <List style={{width: 250}}>
+                     {this.renderMenuList(links)}
+                  </List>
+               </div>
+            </Drawer>
          </>
       );
    }
